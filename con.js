9ccc1ec4bb992d53e2a3dcbd2c0c4cbdc0d6e02c
Sara <sado0001@stud.kea.dk>
2024-02-16 10:40:52 +0100

    java

---
+++ con.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const burgerMenu = document.querySelector(".burger-menu");
+    const menu = document.querySelector(".menu");
+    const bars = document.querySelectorAll(".bar");
+  
+    burgerMenu.addEventListener("click", function () {
+      menu.classList.toggle("show");
+      bars.forEach((bar) => bar.classList.toggle("change"));
+    });
+  });
+  
+  fetch("https://cqapoavevilyrkdkezmw.supabase.co/rest/v1/items", {
+    method: "GET",
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxYXBvYXZldmlseXJrZGtlem13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4NDIzNzMsImV4cCI6MjAyMzQxODM3M30._5AZFrfvIxiCfPmfJg0T0chsj0vkiMa_Od63AQQCg3c",
+    },
+  })
+    .then((res) => res.json())
+    .then(showItems);
+  
+  function showItems(items) {
+    items.forEach(showItem);
+  }
+  
+  function showItem(item) {
+    console.log(item);
+    console.log("hej");
+  
+    const template = document.querySelector("template").content;
+    const clone = template.cloneNode(true);
+  
+    clone.querySelector(".plants_by_1").querySelector("a").textContent =
+      item.title;
+  
+    if (item.sankelandskaber == "Decidous forest") {
+      clone
+        .querySelector(".plants_by_1")
+        .querySelector("a")
+        .classList.add(".remove");
+    } else {
+      clone
+        .querySelector(".plants_by_1")
+        .querySelector("a")
+        .classList.add(".show");
+    }
+  
+    document.querySelector("article").appendChild(clone);
+  
+    if (product.discount) {
+      copy.querySelector(".sale").textContent = product.discount + "%";
+    } else {
+      copy.querySelector(".sale").remove();
+    }
+  }
+  

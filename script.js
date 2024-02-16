f90b27c97287a9fd5093c78a8d640711fea43c36
Sara <sado0001@stud.kea.dk>
2024-02-16 11:22:04 +0100

    burgermenu samlet

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const burgerMenu = document.querySelector(".burger-menu");
-  const menu = document.querySelector(".menu");
-  const bars = document.querySelectorAll(".bar");
-
-  burgerMenu.addEventListener("click", function () {
-    menu.classList.toggle("show");
-    bars.forEach((bar) => bar.classList.toggle("change"));
-  });
-});
-
 fetch("https://cqapoavevilyrkdkezmw.supabase.co/rest/v1/items", {
   method: "GET",
   headers: {

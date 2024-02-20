5a5e6448c4c4865930554396622e3a0689c590c3
Sara <sado0001@stud.kea.dk>
2024-02-20 14:04:24 +0100

    forskelligt

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function fetchData() {
   fetch("https://cqapoavevilyrkdkezmw.supabase.co/rest/v1/items", {
     method: "GET",
     headers: {
-      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxYXBvYXZldmlseXJrZGtlem13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4NDIzNzMsImV4cCI6MjAyMzQxODM3M30._5AZFrfvIxiCfPmfJg0T0chsj0vkiMa_Od63AQQCg3c",
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxYXBvYXZldmlseXJrZGtlem13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4NDIzNzMsImV4cCI6MjAyMzQxODM3M30._5AZFrfvIxiCfPmfJg0T0chsj0vkiMa_Od63AQQCg3c",
     },
   })
     .then((res) => res.json())
@@ -19,7 +20,7 @@ function fetchData() {
 }
 
 function setEventListeners() {
-  document.querySelector("#month").addEventListener("change", showItems)
+  document.querySelector("#month").addEventListener("change", showItems);
 }
 
 function sammenlignTitler(a, b) {
@@ -40,7 +41,6 @@ function saveData(items) {
   showItems();
 }
 
-
 function showItems() {
   document.querySelector(".col_2").innerHTML = "";
   let filteredItems;
@@ -55,12 +55,13 @@ function showItems() {
 function filterItems(items) {
   let selectedMonth = document.querySelector("#month").value;
 
-  if(selectedMonth !== "-1") {
-      return items.filter(item => item.months.some(month => month === selectedMonth));
+  if (selectedMonth !== "-1") {
+    return items.filter((item) =>
+      item.months.some((month) => month === selectedMonth)
+    );
   }
   return items;
 }
-
 
 function showItem(item) {
   const template = document.querySelector("template").content;
@@ -68,8 +69,14 @@ function showItem(item) {
 
   clone.querySelector(".plants_by_1").querySelector("a").textContent =
     item.title;
-  
-  clone.querySelector(".plants_by_1").querySelector("a").setAttribute("href", `plant.html?id=${item.id}`);
+
+  clone.querySelector(".plants_by_1").querySelector("img").src =
+    item.profile_image;
+
+  clone
+    .querySelector(".plants_by_1")
+    .querySelector("a")
+    .setAttribute("href", `plant.html?id=${item.id}`);
 
   if (item.sankelandskaber == "Coniferous forest") {
     clone.querySelector("section").remove();
@@ -77,4 +84,3 @@ function showItem(item) {
     document.querySelector(".col_2").appendChild(clone);
   }
 }
-
